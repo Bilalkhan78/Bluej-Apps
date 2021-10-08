aed48844aacef382ce9f136bfe66e2c00887bb0a
Bilalkhan78 <91319417+Bilalkhan78@users.noreply.github.com>
2021-10-08 12:29:08 +0100

    changed app01

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author Bilal Khan
+ * @version 8/10/2021
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own 
+    private String code;
+    private String title;
+    private int credit;
+    
+
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+        
+    }
+    
+
+}
+    
